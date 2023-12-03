b6ea7d81f907fa3e24efbd8e1725deb205ae300f
maxh8086 <48318337+maxh8086@users.noreply.github.com>
2023-12-03 21:42:47 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,57 @@
+provider "kubernetes" {
+  kubeconfig = file(var.kubeconfig_path)
+}
+
+resource "kubernetes_deployment" "apache" {
+  metadata {
+    name = "apache-deployment"
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "apache"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "apache"
+        }
+      }
+
+      spec {
+        container {
+          image = "${var.docker_image}:${var.docker_tag}"
+          name  = "apache"
+          ports {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "apache" {
+  metadata {
+    name = "apache-service"
+  }
+
+  spec {
+    selector = {
+      app = "apache"
+    }
+
+    port {
+      port        = 80
+      target_port = 80
+      node_port   = 30080
+    }
+
+    type = "NodePort"
+  }
+}
