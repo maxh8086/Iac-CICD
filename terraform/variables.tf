343e9a98874265322ec5023f413441bef29dd80f
maxh8086 <48318337+maxh8086@users.noreply.github.com>
2023-12-03 23:04:15 +0530

    updates

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,14 @@
+variable "DOCKER_IMAGE" {
+  description = "Docker image for the Kubernetes deployment"
+  type        = string
+}
+
+variable "DOCKER_TAG" {
+  description = "Docker tag for the Kubernetes deployment"
+  type        = string
+}
+
 locals {
-  kubeconfig = var.kubeconfig_path
+  kubeconfig_path = "${var.KUBECONFIG}"
   deployment_image = "${var.DOCKER_IMAGE}:${var.DOCKER_TAG}"
 }
