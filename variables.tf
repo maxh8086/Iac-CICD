7fdc434dc9456779c396a5eb3c653b8182e675b8
maxh8086 <48318337+maxh8086@users.noreply.github.com>
2023-12-03 22:32:06 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "kubeconfig_path" {
   description = "Path to the kubeconfig file"
   type        = string
-  default     = var.KUBECONFIG
+  default     = KUBECONFIG
 }
 
 variable "deployment_image" {
   description = "Docker image for the Kubernetes deployment"
   type        = string
-  default     = "${var.DOCKER_IMAGE}:${var.DOCKER_TAG}"
+  default     = "${DOCKER_IMAGE}:${DOCKER_TAG}"
 }
