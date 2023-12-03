4b53db69c6323cad9c9dc07240b8decf3d169fa9
maxh8086 <48318337+maxh8086@users.noreply.github.com>
2023-12-03 23:15:59 +0530

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,7 +24,7 @@ resource "kubernetes_deployment" "apache" {
 
       spec {
         container {
-          image = deployment_image
+          image = var.deployment_image
           name  = "apache"
           ports {
             container_port = 80
